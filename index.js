aa9bb8037134a1ca53cf55c99e1391b4e2aa3cff
njihun <102475695+njihun@users.noreply.github.com>
2024-08-10 21:35:23 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import { drawingLogo, deviceType } from "./module.js";
-document.addEventListener('load', () => {
-    document.getElementById('insta').src = drawingLogo().toDataURL('image/jpeg');
-    let social = document.querySelectorAll('.social > a');
-    let insta = social[0];
-    switch (deviceType) {
-        case 'android':
-            insta.href = "intent://instagram.com/#Intent;package=com.instagram.android;scheme=https;end";
-            break;
-        case 'ios':
-            insta.href = "instagram://media";
-            break;
-                
-        default:
-            insta.href = 'https://instagram.com/move._.on__';
-            break;
-    }
-});
+
+document.getElementById('insta').src = drawingLogo().toDataURL('image/jpeg');
+let social = document.querySelectorAll('.social > a');
+let insta = social[0];
+switch (deviceType) {
+    case 'android':
+        insta.href = "intent://instagram.com/#Intent;package=com.instagram.android;scheme=https;end";
+        break;
+    case 'ios':
+        insta.href = "instagram://media";
+        break;
+            
+    default:
+        insta.href = 'https://instagram.com/move._.on__';
+        break;
+}
